e34fe024eb75dc9eb20ebb307adef619bab29d87
Querne <jeremyquerne@hotmail.fr>
2021-09-28 16:58:48 +0200

    routes, jumbotron, overnav infos, navbar and footer, first try

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,8 +1,26 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const NotFound = () => {
 
   return (
-    <section>
-      <h2>...Not Found...</h2>
+    <section class="page_404">
+      <Container className="container">
+        <Row className="row"> 
+          <Col className="col text-center">
+            <div className="four_zero_four_bg">
+              <h1 className="text-center ">404</h1>
+            </div>
+            <div className="contant_box_404">
+              <h3 className="h2">
+              Look like you're lost
+              </h3>
+              <p>the page you are looking for not avaible!</p>
+              <Link to="/" class="link_404">Home</Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
---
+++ src/locales/fr/translation_fr.js
@@ -0,0 +1,16 @@
+export const TRANSLATIONS_FR = {
+    //header
+    address1: "8 allée des violettes 29600 Saint-Martin-Des-Champs", 
+    email: "lamaisonblanche@gmail.com",
+
+    //navlink
+    home:"Accueil",
+
+    //homepage
+
+    //Booking
+    booking:"Réservez",
+
+    //AboutUs
+    aboutus:"A Propos",
+};
