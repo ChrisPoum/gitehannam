f47fc1834f06dfc982ab9a6144d0424f95602497
Querne <jeremyquerne@hotmail.fr>
2021-09-29 17:18:03 +0200

    adding links and style to the footer

--- src/locales/en/translation_en.js
+++ src/locales/en/translation_en.js
@@ -1,12 +1,16 @@
 export const TRANSLATIONS_EN = {
-    //header
+    //header & footer
     address1: "15 allée des violettes 29600 Saint-Martin-Des-Champs",
     email: "lamaisonblanche@gmail.com",
+    quicklinks: "Quick Links",
+    socialnetwork:"Follow Us",
+    callus:"Call Us",
+    findus:"Find Us",
     
     //navlink
     home:"Home",
 
-    //home
+    //homepage
 
     //Booking
     booking:"Book Now",
